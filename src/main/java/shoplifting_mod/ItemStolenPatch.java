83dba531ac83ea6122e5fc5f28ce2a31b3c458fd
MMagicala <36617521+MMagicala@users.noreply.github.com>
2020-09-16 10:26:24 -0700

    base functionality works

---
+++ src/main/java/shoplifting_mod/ItemStolenPatch.java
@@ -0,0 +1,142 @@
+package shoplifting_mod;
+
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.shop.ShopScreen;
+import com.megacrit.cardcrawl.shop.StorePotion;
+import com.megacrit.cardcrawl.shop.StoreRelic;
+import com.megacrit.cardcrawl.vfx.DarkSmokePuffEffect;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+
+import java.util.ArrayList;
+
+public class ItemStolenPatch {
+    @SpirePatch(
+            clz = ShopScreen.class,
+            method = "purchaseCard"
+    )
+    @SpirePatch(
+            clz = StorePotion.class,
+            method = "purchasePotion"
+    )
+    @SpirePatch(
+            clz = StoreRelic.class,
+            method = "purchaseRelic"
+    )
+    public static class MainPatch {
+        private static final ArrayList<String> METHOD_NAMES = new ArrayList<String>() {
+            {
+                add("play");
+                add("addShopPurchaseData");
+                add("playBuySfx");
+                add("createSpeech");
+            }
+        };
+        private static boolean isCreateSpeechPatched = false;
+        private static String savedFileName = null;
+
+        public static ExprEditor Instrument() {
+            return new ExprEditor() {
+                @Override
+                public void edit(MethodCall m) throws CannotCompileException {
+                    String currentFileName = m.getFileName();
+                    if (savedFileName == null || !savedFileName.equals(m.getFileName())) {
+                        savedFileName = currentFileName;
+                        // Reset flag since we're patching a new class
+                        isCreateSpeechPatched = false;
+                    }
+                    String methodName = m.getMethodName();
+                    if (METHOD_NAMES.contains(methodName) && !isCreateSpeechPatched) {
+                        m.replace(getExpr(currentFileName, methodName));
+                        // Don't patch any next createSpeech method calls
+                        if (methodName.equals("createSpeech")) {
+                            isCreateSpeechPatched = true;
+                        }
+                    }
+                }
+            };
+        }
+
+        private static String getExpr(String fileName, String methodName) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("if(!");
+            sb.append(ShopliftingMod.class.getName());
+            sb.append(".isItemStolen()){ $_ = $proceed($$);");
+            if (methodName.equals("createSpeech")) {
+                sb.append("}else{");
+                // Play gold jingle and smoke sound
+                sb.append(CardCrawlGame.class.getName());
+                sb.append(".sound.play(\"GOLD_JINGLE\");");
+                // Play smoke vfx at the saved x and y
+                sb.append(AbstractDungeon.class.getName());
+                sb.append(".topLevelEffectsQueue.add(new ");
+                sb.append(DarkSmokePuffEffect.class.getName());
+                sb.append("(");
+                sb.append(ShopliftingMod.class.getName());
+                sb.append(".prevItemX, ");
+                sb.append(ShopliftingMod.class.getName());
+                sb.append(".prevItemY));");
+                // Reset flag
+                sb.append(ShopliftingMod.class.getName());
+                sb.append(".resetFlag();");
+            } else if (methodName.equals("addShopPurchaseData")) {
+                // Save x and y for smoke fx later
+                sb.append("}else{");
+                // Determine class of item to get the right field
+                if (fileName.equals("StoreRelic.java")) {
+                    addSaveCoordinatesExpr(sb, "relic.current", true);
+                } else if (fileName.equals("ShopScreen.java")) {
+                    addSaveCoordinatesExpr(sb, "hoveredCard.current_", false);
+                }
+            }
+            sb.append("}");
+            return sb.toString();
+        }
+
+        // y = false -> x
+        // uppercase = false -> lowercase
+        private static void addSaveCoordinatesExpr(StringBuilder sb, String propertyPrefix, boolean uppercase) {
+            char uppercaseCoordinate = 'X';
+            char coordinate = uppercase ? 'X' : 'x';
+            for (int i = 0; i < 2; i++) {
+                sb.append(ShopliftingMod.class.getName());
+                sb.append(".prevItem");
+                sb.append(uppercaseCoordinate);
+                sb.append(" = ");
+                sb.append(propertyPrefix);
+                sb.append(coordinate);
+                sb.append(';');
+                coordinate++;
+                uppercaseCoordinate++;
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = StorePotion.class,
+            method = "purchasePotion"
+    )
+    public static class SavePotionCoordinatePatch{
+        private static class PotionBeforeMoveLocator extends SpireInsertLocator {
+            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+                Matcher matcher = new Matcher.MethodCallMatcher(AbstractPlayer.class, "obtainPotion");
+                return LineFinder.findInOrder(ctMethodToPatch, matcher);
+            }
+        }
+
+        @SpireInsertPatch(
+                locator = PotionBeforeMoveLocator.class
+        )
+        public static void Insert(StorePotion __instance){
+            ShopliftingMod.prevItemX = __instance.potion.posX;
+            ShopliftingMod.prevItemY = __instance.potion.posY;
+        }
+    }
+}
---
+++ src/main/java/shoplifting_mod/ShopliftingMod.java
@@ -0,0 +1,235 @@
+/*
+MIT License
+
+Copyright (c) 2020 MMagicala
+Copyright (c) 2018 t-larson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package shoplifting_mod;
+
+import basemod.*;
+import basemod.interfaces.PostInitializeSubscriber;
+import basemod.interfaces.RenderSubscriber;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.shop.ShopScreen;
+import com.megacrit.cardcrawl.shop.StorePotion;
+import com.megacrit.cardcrawl.shop.StoreRelic;
+import com.megacrit.cardcrawl.vfx.combat.DamageImpactBlurEffect;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+@SpireInitializer
+public class ShopliftingMod implements PostInitializeSubscriber {
+    private static SpireConfig config;
+
+    // Stats
+    private static final float successRate = 0.4f;
+    private static final int damageAmount = 25;
+
+    private static boolean isItemStolen = false;
+    public static float prevItemX, prevItemY;
+    private static boolean isKickedOut = false;
+
+    // Mod UI
+    private static final String HOTKEY_KEY = "hotkey";
+    private static final float BUTTON_X = 350.0f;
+    private static final float BUTTON_Y = 650.0f;
+    private static final float BUTTON_LABEL_X = 475.0f;
+    private static final float BUTTON_LABEL_Y = 700.0f;
+
+    private static final String[] DIALOGUE = {"Thief!", "Hey! No stealing!", "Do you want me to kick you out?"};
+
+    public ShopliftingMod() {
+        System.out.println("Shoplifting Mod initialized");
+    }
+
+    public static void initialize() {
+        BaseMod.subscribe(new ShopliftingMod());
+    }
+
+    @Override
+    public void receivePostInitialize() {
+        // Define default properties
+        Properties properties = new Properties();
+        properties.setProperty(HOTKEY_KEY, Integer.toString(Input.Keys.X));
+
+        // Try to load a config file. If not found, use the default properties
+        try {
+            config = new SpireConfig("ShopliftingMod", "config", properties);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // create mod settings panel
+        ModPanel settingsPanel = new ModPanel();
+
+        ModLabel buttonLabel = new ModLabel("", BUTTON_LABEL_X, BUTTON_LABEL_Y, settingsPanel, (me) -> {
+            if (me.parent.waitingOnEvent) {
+                me.text = "Press key";
+            } else {
+                me.text = "Change shoplifting hotkey (" + Input.Keys.toString(config.getInt(HOTKEY_KEY)) + ")";
+            }
+        });
+        settingsPanel.addUIElement(buttonLabel);
+
+        ModButton hotkeyButton = new ModButton(BUTTON_X, BUTTON_Y, settingsPanel, (me) -> {
+            me.parent.waitingOnEvent = true;
+            InputProcessor oldInputProcessor = Gdx.input.getInputProcessor();
+            Gdx.input.setInputProcessor(new InputAdapter() {
+                @Override
+                public boolean keyUp(int keycode) {
+                    config.setInt(HOTKEY_KEY, keycode);
+                    me.parent.waitingOnEvent = false;
+                    Gdx.input.setInputProcessor(oldInputProcessor);
+                    return true;
+                }
+            });
+        });
+        settingsPanel.addUIElement(hotkeyButton);
+
+        // Load config
+        BaseMod.registerModBadge(ImageMaster.loadImage("badge.jpg"), "Best Route Mod", "MMagicala", "Find the best route in the map!", settingsPanel);
+    }
+
+    @SpirePatch(
+            clz = ShopScreen.class,
+            method = "update"
+    )
+    public static class StealCardPatch {
+        @SpireInsertPatch(
+                locator = ItemHoveredCodeLocator.class,
+                localvars = {"hoveredCard"}
+        )
+        public static SpireReturn<Void> Insert(Object __instance, AbstractCard hoveredCard) {
+            return CommonInsert(__instance, hoveredCard);
+        }
+    }
+
+    @SpirePatch(
+            clz = StorePotion.class,
+            method = "update"
+    )
+    @SpirePatch(
+            clz = StoreRelic.class,
+            method = "update"
+    )
+    public static class StealPotionOrRelicPatch {
+        @SpireInsertPatch(
+                locator = ItemHoveredCodeLocator.class
+        )
+        public static SpireReturn<Void> Insert(Object __instance) {
+            return CommonInsert(__instance, null);
+        }
+    }
+
+    private static class ItemHoveredCodeLocator extends SpireInsertLocator {
+        public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+            Matcher matcher = new Matcher.MethodCallMatcher(ShopScreen.class, "moveHand");
+            return LineFinder.findAllInOrder(ctMethodToPatch, matcher);
+        }
+    }
+
+    private static SpireReturn<Void> CommonInsert(Object __instance, AbstractCard hoveredCard){
+        int itemPrice = -1;
+        if (__instance instanceof StoreRelic) {
+            itemPrice = ((StoreRelic) __instance).price;
+        } else if (__instance instanceof StorePotion) {
+            itemPrice = ((StorePotion) __instance).price;
+        }else if(__instance instanceof ShopScreen){
+            itemPrice = hoveredCard.price;
+        }
+        if (AbstractDungeon.player.gold < itemPrice
+                && Gdx.input.isKeyJustPressed(config.getInt(HOTKEY_KEY))) {
+            // Attempt to steal the item
+            Random random = new Random();
+            float rollResult = random.nextFloat();
+            if (rollResult < successRate) {
+                // Success! Give the player money to purchase the item
+                AbstractDungeon.player.gold += itemPrice;
+                isItemStolen = true;
+                if (__instance instanceof StoreRelic) {
+                    ((StoreRelic)__instance).purchaseRelic();
+                } else if (__instance instanceof StorePotion) {
+                    ((StorePotion)__instance).purchasePotion();
+                }else if(__instance instanceof ShopScreen){
+                    try {
+                        Method purchaseCardMethod = ShopScreen.class.getDeclaredMethod("purchaseCard", AbstractCard.class);
+                        if(!purchaseCardMethod.isAccessible()){
+                            purchaseCardMethod.setAccessible(true);
+                        }
+                        purchaseCardMethod.invoke(__instance, hoveredCard);
+                    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                // Take damage if caught and play sound/vfx
+                AbstractDungeon.player.damage(new DamageInfo(null, damageAmount, DamageInfo.DamageType.NORMAL));
+                int coin = random.nextInt(2);
+                String key = coin == 1 ? "BLUNT_FAST" : "BLUNT_HEAVY";
+                CardCrawlGame.sound.play(key);
+//                AbstractDungeon.topLevelEffectsQueue.add(new DamageImpactBlurEffect())
+
+                // Choose random shopkeeper dialogue
+                int index = random.nextInt(DIALOGUE.length);
+                AbstractDungeon.shopScreen.createSpeech(DIALOGUE[index]);
+
+                // Kick player out of shop if they are alive
+                if(!AbstractDungeon.player.isDead) {
+                    AbstractDungeon.closeCurrentScreen();
+                    isKickedOut = true;
+                }
+            }
+
+            // Return early
+            return SpireReturn.Return(null);
+        }
+        // Hotkey not pressed, return to normal
+        return SpireReturn.Continue();
+    }
+
+    // Getter/setter methods
+
+    public static void resetFlag() {
+        isItemStolen = false;
+    }
+
+    public static boolean isItemStolen() {
+        return isItemStolen;
+    }
+}
