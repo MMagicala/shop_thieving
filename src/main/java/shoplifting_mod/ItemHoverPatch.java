9df84a928d7978e8ac7480eb253355b1cf4190af
MMagicala <36617521+MMagicala@users.noreply.github.com>
2020-09-23 21:29:13 -0700

    add hover tip tool text

--- src/main/java/shoplifting_mod/ItemHoverPatch.java
+++ src/main/java/shoplifting_mod/ItemHoverPatch.java
@@ -1,5 +1,6 @@
 package shoplifting_mod;
 
+import basemod.ReflectionHacks;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,6 +11,7 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.GameCursor;
 import com.megacrit.cardcrawl.core.OverlayMenu;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
 import com.megacrit.cardcrawl.shop.Merchant;
@@ -52,5 +54,50 @@ public class ItemHoverPatch {
             Matcher matcher = new Matcher.MethodCallMatcher(ShopScreen.class, "moveHand");
             return LineFinder.findAllInOrder(ctMethodToPatch, matcher);
         }
+    }
+
+    @SpirePatch(clz = CardCrawlGame.class, method = "render")
+    public static class RenderToolTipTextPatch {
+/*
+        @SpireInsertPatch(
+                locator=DisplayCursorLocator.class
+        )
+        public static void InsertBeforeBlackFadeScreen(CardCrawlGame __instance){
+            if (itemHovered) {
+                AbstractDungeon.overlayMenu.showBlackScreen();
+            }
+        }
+*/
+
+        @SpireInsertPatch(
+                locator=RenderBlackScreenLocator.class
+        )
+        public static void InsertAfterBlackFadeScreen(CardCrawlGame __instance) {
+            if (itemHovered) {
+                float x = InputHelper.mX;
+                float y = InputHelper.mY - 64;
+                SpriteBatch sb = (SpriteBatch)ReflectionHacks.getPrivate(__instance, CardCrawlGame.class, "sb");
+                FontHelper.renderFontLeft(sb, FontHelper.bannerFont, "Steal item?", x, y, Color.WHITE);
+                itemHovered = false;
+            }
+        }
+
+        // Locators
+
+        private static class RenderBlackScreenLocator extends SpireInsertLocator {
+            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+                Matcher matcher = new Matcher.MethodCallMatcher(CardCrawlGame.class, "renderBlackFadeScreen");
+                return LineFinder.findAllInOrder(ctMethodToPatch, matcher);
+            }
+        }
+/*
+
+        private static class DisplayCursorLocator extends SpireInsertLocator {
+            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+                Matcher matcher = new Matcher.FieldAccessMatcher(CardCrawlGame.class, "displayCursor");
+                return LineFinder.findAllInOrder(ctMethodToPatch, matcher);
+            }
+        }
+*/
     }
 }
