7921ee21cc657cc74d32e50a2ab02b198da70bd8
MMagicala <36617521+MMagicala@users.noreply.github.com>
2020-10-05 22:24:15 -0700

    wip

---
+++ src/main/java/shoplifting_mod/handlers/ShopEffectHandler.java
@@ -0,0 +1,16 @@
+package shoplifting_mod.handlers;
+
+import com.megacrit.cardcrawl.vfx.combat.SmokeBombEffect;
+
+import java.util.ArrayList;
+
+public class ShopEffectHandler {
+    private static final ArrayList<SmokeBombEffect> customEffects = new ArrayList<>();
+    public static void playStolenEffect(float x, float y){
+        // customEffects.add();
+    }
+
+    public static void updateCustomEffectsList(){
+        // customEffects.removeIf();
+    }
+}
