b5059ad814a73aba5f284a33ed344fbc51eccc92
MMagicala <36617521+MMagicala@users.noreply.github.com>
2020-10-10 15:40:50 -0700

    add lose relic and potion punishment

--- src/main/java/thieving_mod/Punishment.java
+++ src/main/java/thieving_mod/Punishment.java
@@ -2,9 +2,14 @@ package thieving_mod;
 
 public enum Punishment {
     CURSES("May this curse stick with you for the rest of your journey..."),
-    LOSE_ALL_GOLD("You tried to steal from me. Now I will do the same to you..."),
-    BLIGHT("Have this blight instead!");
-
+    LOSE_ALL_GOLD("Give me all your gold!"),
+    BLIGHT("Have this blight instead!"),
+    LOSE_CARD("You tried to steal from me?",
+            "I'll steal something from you too!"),
+    LOSE_RELIC("You tried to steal from me?",
+            "I'll steal something from you too!"),
+    LOSE_POTION("You tried to steal from me?",
+            "I'll steal something from you too!");
     public String[] dialogue;
 
     Punishment(String... dialogue) {
